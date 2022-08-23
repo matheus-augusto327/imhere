a56bdfbabac79d9dac6e19c28f76779348934f0c
Matheus <matheus.augustoreal@gmail.com>
2022-08-23 16:28:06 -0300

    :tada: First commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,41 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+import { Participant } from "../../components/Participant/";
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar!")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        React Native
+      </Text>
+
+      <Text style={styles.eventDate}>
+        React Native
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          keyboardType="numeric"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+    </View>
+  )
+}
